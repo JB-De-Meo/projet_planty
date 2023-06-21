a12067ccdb5ffd122784ff8f9adcfa8d20924ae9
JB De Meo <connectionlocale@gmail.com>
2023-06-21 10:40:36 +0200

    save planty 7

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -1,3 +1,4 @@
+       
 <?php
 /**
  * Astra Child Theme functions and definitions
@@ -13,13 +14,29 @@
  */
 define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
 
+add_action('wp_footer', 'afficher_emplacements_de_menu');
+
 /**
  * Enqueue styles
  */
 function child_enqueue_styles() {
-
-	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-
+    wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 }
-
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+/**
+ * Ajouter le lien d'administration au menu
+ */
+function ajouter_lien_admin_menu($items, $args) {
+    // Vérifier si l'utilisateur est connecté et a le rôle d'administrateur
+    if (is_user_logged_in() && $args->theme_location === 'primary') {
+        // Créer le lien d'administration
+        $admin_link = '<li class="menu-item menu-item-type-custom">';
+        $admin_link .= '<a href="' . admin_url() . '">Administrator</a>';
+        $admin_link .= '</li>';
+        // Ajouter le lien d'administration au menu
+        $items .= $admin_link;
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin_menu', 10, 2);
